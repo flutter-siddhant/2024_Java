49f1fce5b514f00439d0953dabfb9019c498483a
Siddhant Satkar <flutter.siddhantsatkar@gmail.com>
2024-03-01 20:33:40 +0530

    Accessing the array elements

---
+++ Array2.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Array2 {
+
+	public static void main(String[] args) {
+		int arr [] = {1,2,3,4,5};
+		String god [] = {"Siddhant", "Radha", "Krishna"};
+		float pointer [] = {9.70f,9.55f,9.39f};
+		boolean check [] = {true, false, true};
+		
+		System.out.println(arr[4]);
+		System.out.println(god[1]);
+		System.out.println(pointer[0]);
+		System.out.println(check[2]);
+	}
+
+}
